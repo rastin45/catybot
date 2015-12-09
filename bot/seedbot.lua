a12139dec6ed251d9c5b6045e02e5a9d6c51ca2b
rastin45 <miladworld514@gmail.com>
2015-12-09 20:36:43 +0000

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -219,7 +219,7 @@ function create_config( )
     "download_media",
     "invite"
     },
-    sudo_users = {176229065},--Sudo users
+    sudo_users = {146172170},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
